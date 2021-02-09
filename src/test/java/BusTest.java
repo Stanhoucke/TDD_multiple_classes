6277ccb9d4effe02328c0693ce392a9da38b9239
Stanhoucke <stanhoucke@gmail.com>
2021-02-09 14:54:24 +0000

    bus has destination

--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BusTest {
+   private Bus bus;
+
+   @Before
+    public void before(){
+       bus = new Bus("Joppa");
+   }
+
+   @Test
+    public void hasDestination(){
+       assertEquals("Joppa", bus.getDestination());
+   }
 }
